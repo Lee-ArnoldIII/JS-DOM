8952610e65760276719655749484b498e7fd61f9
Lee Arnold <lee.arnoldiii@gmail.com>
2018-10-23 18:31:52 -0500

    getting started

--- ex2/script.js
+++ ex2/script.js
@@ -1,1 +1,3 @@
 // Change the background color of the box to blue
+let colorChange = document.getElementById('box')
+colorChange.style.backgroundColor = 'blue'
--- ex3/script.js
+++ ex3/script.js
@@ -1,7 +1,8 @@
 // Make the box blue when you click the button. (Here's some code to help you get started)
 
-var button = document.getElementById("myButton");
+var button = document.getElementById('myButton')
+let color = document.getElementById('box')
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function() {
     
 })
